0b532628513fdea8430b27ffcfe9d49dd6c272b6
mccawm9238 <mccawm@faytechcc.edu>
2026-02-22 12:36:41 -0500

    Add P1LAB1 assignment

---
+++ P1LAB1_McCawMegan.py
@@ -0,0 +1,10 @@
+
+# Megan McCaw
+# February 21, 2026
+# P1LAB1
+# This program will display a welcome message to the user
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Welcome to CTI-110, " + first_name + " " + last_name + "!")
